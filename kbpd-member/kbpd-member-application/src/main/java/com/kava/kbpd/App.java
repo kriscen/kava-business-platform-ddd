77d8b735e1d0fffc6a2bcc7aab429e37d0a181fe
Kris <kriscen@163.com>
2025-09-16 14:29:19 +0800

    feat(*): ddd项目结构调整
    ddd项目结构调整

---
+++ kbpd-member/kbpd-member-application/src/main/java/com/kava/kbpd/App.java
@@ -0,0 +1,13 @@
+package com.kava.kbpd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
