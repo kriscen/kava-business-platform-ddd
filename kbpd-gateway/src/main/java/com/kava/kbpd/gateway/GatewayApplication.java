42d7459a87708829675fa02dab8a4189f6ab895e
Kris <kriscen@163.com>
2026-01-21 16:47:47 +0800

    feat(gateway): 新增gateway
    新增gateway

--- kbpd-gateway/src/main/java/com/kava/kbpd/gateway/GatewayApplication.java
+++ kbpd-gateway/src/main/java/com/kava/kbpd/gateway/GatewayApplication.java
@@ -1,9 +1,18 @@
 package com.kava.kbpd.gateway;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author Kris
  * @date 2025/3/31
  * @description: gateway 启动类
  */
+@Slf4j
+@SpringBootApplication
 public class GatewayApplication {
+    public static void main(String[] args){
+        SpringApplication.run(GatewayApplication.class, args);
+    }
 }
